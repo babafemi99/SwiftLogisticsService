fd320b73fb4fb9a4f35256053411d8ad8080e06e
Oreoluwa <oloniyo.oreoluwa@adexfe.com>
2022-09-01 23:09:19 +0100

    fixed test

--- internal/datasource/psqlSrc/psql_test.go
+++ internal/datasource/psqlSrc/psql_test.go
@@ -12,6 +12,9 @@ import (
 func TestNewPsqlSrc(t *testing.T) {
 	log := logrus.New()
 	psq, _ := NewPsqlSrc(log, "postgres://postgres:mysecretpassword@localhost:5432/slsstore")
+	err := psq.LoadDB("./../create.sql")
+
+	require.NoError(t, err)
 	type args struct {
 		log     *logrus.Logger
 		connStr string
